312784d63a9d56206eb14e72169920aff55d20bd
HyeonSeuk <seo5719@naver.com>
2023-05-23 23:17:55 +0900

    되라

--- pjt/settings.py
+++ pjt/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['port-0-video-test-otjl2cli0afqfi.sel4.cloudtype.app']
 
 
 # Application definition
